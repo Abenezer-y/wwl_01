58b902a58a655fe3f6ebf04571523e7f7b40aefe
Ayonathan <ayonathan0@gmai.com>
2022-11-08 23:29:00 +0300

    vgc

--- src/components/duty_table.jsx
+++ src/components/duty_table.jsx
@@ -3,8 +3,9 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Form from 'react-bootstrap/Form';
 import React, {Component} from 'react';
-import './main.css'
+import axios from 'axios';
 
+// 
 
 class DutyTable extends Component {
   state = { status: ['checked', '', ''], 'id': [0, 1, 2],time: '15:30', detail: 'Change chiller setpoint to -13/-14', duration: '', support: '', data: [['15:30', 'Change chiller setpoint to -13/-14', '', ''], ['15:30', 'Chiller setpoint', '4', ''], ['', 'Change', '', 'Dan']] } 
@@ -15,11 +16,58 @@ class DutyTable extends Component {
     return x
   };
 
+  // async getDataAxios(){
+  //   
+  //                
+  //   var config = {
+  //     method: 'post',
+  //     url: 'https://data.mongodb-api.com/app/data-qvnrx/endpoint/data/v1/action/findOne',
+  //     headers: {'Content-Type': 'application/json', 'Access-Control-Request-Headers': '*', 'api-key': '5d9sGO28viSiX1HnJlOLN6QMqPqxYz6NIKVUMvEU8wXvAS0CPHMMHs2jF0UHKSCF'},
+  //     data: data
+  //   };
+
+    
+
+    // console.log(response.data)
+    // const response =
+    //   await axios(config).then(function (response) {console.log(JSON.stringify(response.data));}).catch(function (error) {console.log(error);})
+    
+
+  componentDidMount () {
+    // var axios = require('axios');
+    var data = JSON.stringify({
+        "collection": "duty",
+        "database": "wwl",
+        "dataSource": "WWL",
+        "projection": {
+            "id": 1
+        }
+    });
+                
+    var config = {
+        method: 'post',
+        url: 'https://data.mongodb-api.com/app/data-qvnrx/endpoint/data/v1/action/findOne',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Request-Headers': '*',
+          'api-key': '5d9sGO28viSiX1HnJlOLN6QMqPqxYz6NIKVUMvEU8wXvAS0CPHMMHs2jF0UHKSCF',
+        },
+        data: data
+    };
+                
+    axios(config)
+        .then(function (response) {
+            console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
 
   handleCheck = (x) => {
-    console.log(x)
+    // console.log(x)
     let new_stat = this.state.status
-
+  
     if (this.state.status[x] === 'checked')
       {new_stat[x] = ''
       this.setState({status: new_stat})
@@ -47,11 +95,11 @@ class DutyTable extends Component {
   renderTable = (i) => {
     if (this.state.status[i] === 'checked') 
       return     <>
-      <th id={i} onChange={this.handleClick} scope="row"><Form.Check style={{width:30, height:30}} type="checkbox" class="custom-control-input" id="customCheck1" defaultChecked/></th>
+      <th id={i} onChange={this.handleClick} scope="row"><Form.Check type="checkbox" class="custom-control-input" id="customCheck1" defaultChecked/></th>
       {this.renderSelected(this.state.data[i])}
   </>
     return   <>
-    <th id={i} onChange={this.handleClick} scope="row"> <Form.Check style={{width:30, height:30}} type="checkbox" class="custom-control-input" id="customCheck1" /></th>
+    <th id={i} onChange={this.handleClick} scope="row"> <Form.Check type="checkbox" class="custom-control-input" id="customCheck1" /></th>
     {this.renderRow(this.state.data[i])}
     </>
   }
@@ -59,10 +107,10 @@ class DutyTable extends Component {
   render() { 
     
     return (
-      <Container>
+      <Container className='me-3zzz'>
     <Row>
-    <h3 className="text-center">Duty Manager Check List</h3>
-    <h4 className="text-center">Nov 03, 2022</h4>
+    <h3>Duty Manager Check List</h3>
+    <h4>Nov 03, 2022</h4>
     </Row>
     <Row >
     <Table >
